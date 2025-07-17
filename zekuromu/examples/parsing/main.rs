c4629cb6478a97aa691378776de383e9caf5925d
ShinySaana <shinysaana@gmail.com>
2025-07-17 10:39:14 +0200

    task: add quick raw to raw merge

--- zekuromu/examples/parsing/main.rs
+++ zekuromu/examples/parsing/main.rs
@@ -4,6 +4,10 @@ use zekuromu;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let first_arg_as_path = Path::new(&args[1]);
+    let second_arg_as_path = Path::new(&args[2]);
 
-    println!("{:?}", zekuromu::raw::parse_file_as_raw_data(&first_arg_as_path).unwrap());
+    let data = zekuromu::raw::parse_file_as_raw_data(&first_arg_as_path).unwrap();
+    let merge = zekuromu::raw::parse_file_as_raw_data(&second_arg_as_path).unwrap();
+
+    println!("{:?}", data.recursive_merge(merge));
 }
--- zekuromu/src/data/mod.rs
+++ zekuromu/src/data/mod.rs
@@ -1,11 +1,13 @@
-use std::collections::HashMap;
+pub mod operators;
 
-#[derive(Debug)]
+use std::collections::{HashMap, HashSet};
 
 // Any serialized format should implement at least these.
 // Explicitely constrains `Mapping` to only use Strings as keys.
 // TODO: `Number` should be arbitrarily large in practice; left as a float for now.
+#[derive(Default, Clone, Debug)]
 pub enum RawData {
+    #[default]
     Null,
     Boolean(bool),
     Number(f64),
@@ -13,3 +15,44 @@ pub enum RawData {
     Sequence(Vec<RawData>),
     Mapping(HashMap<String, RawData>)
 }
+
+impl RawData {
+    pub fn recursive_merge(self, other: RawData) -> RawData {
+        match self {
+            RawData::Mapping(mut self_mapping) => {
+                if let RawData::Mapping(mut other_mapping) = other {
+                    let keys: HashSet<_> = HashSet::from_iter(self_mapping
+                        .keys()
+                        .cloned()
+                        .chain(
+                            other_mapping
+                            .keys()
+                            .cloned()
+                        ));
+
+                    for key in keys {
+                        if let Some(other_inner) = other_mapping.remove(&key) {
+                            if let Some(self_inner) = self_mapping.remove(&key) {
+                                self_mapping.insert(
+                                    key.clone(),
+                                    self_inner.recursive_merge(other_inner.clone())
+                                );
+                            } else {
+                                self_mapping.insert(
+                                    key.clone(),
+                                    other_inner
+                                );
+                            }
+                        }
+                    }
+
+                    RawData::Mapping(self_mapping)
+                } else {
+                    other
+                }
+            },
+            _ => other
+        }
+    }
+}
+
